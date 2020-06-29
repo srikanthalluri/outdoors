e71927a2120763e7ffe73748c521590ca1202dca
srikanth <srikanth.v@exotel.in>
2020-06-29 14:23:27 +0530

    Implemented BEM css methodology

--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ class App extends React.Component {
     return (
       <div>
         <header className="header">
-          <div className="logo-box">
-            <img src={logo} alt="Logo" className="logo" />
+          <div className="header__logo-box">
+            <img src={logo} alt="Logo" className="header__logo" />
           </div>
-          <div className="text-box">
+          <div className="header__text-box">
             <h1 className="heading-primary">
-              <span className="heading-primary-main">Outdoors</span>
-              <span className="heading-primary-sub">is where life happens</span>
+              <span className="heading-primary--main">Outdoors</span>
+              <span className="heading-primary--sub">
+                is where life happens
+              </span>
             </h1>
-            <a href="#" className="btn btn-white btn-animated">
+            <a href="#" className="btn btn--white btn--animated">
               Discover our tours
             </a>
           </div>
