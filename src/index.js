5982eea7e13912ab3b2b0769766bc20ae109e764
Srikanth <srikanth.varma@gmail.com>
2020-06-30 15:45:24 +0530

    Designed one card sample for tours section

--- src/index.js
+++ src/index.js
@@ -163,6 +163,27 @@ class App extends React.Component {
               </div>
             </div>
           </section>
+
+          <section className="section-tours">
+            <div className="u-center-text u-margin-bottom-big">
+              <h1 className="heading-secondary">
+                Exciting tours for adventurous people
+              </h1>
+            </div>
+
+            <div className="row">
+              <div className="col-1-of-3">
+                <div className="card">
+                  <div className="card__side card__side--front"></div>
+                  <div className="card__side card__side--back card__side--back-1">
+                    back
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-of-3"></div>
+              <div className="col-1-of-3"></div>
+            </div>
+          </section>
         </main>
       </div>
     );
