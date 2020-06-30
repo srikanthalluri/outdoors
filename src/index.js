e5f97944ba4d1612a172af9087fb9edc4ee82c05
Srikanth <srikanth.varma@gmail.com>
2020-06-30 22:04:16 +0530

    Completed tours section stylying

--- src/index.js
+++ src/index.js
@@ -181,7 +181,7 @@ class App extends React.Component {
                     </h4>
                     <div className="card__details">
                       <ul>
-                        <li>3 day</li>
+                        <li>3 day tours</li>
                         <li>Up to 30 people</li>
                         <li>2 tour guides</li>
                         <li>Sleep in cozy hotels</li>
@@ -190,12 +190,88 @@ class App extends React.Component {
                     </div>
                   </div>
                   <div className="card__side card__side--back card__side--back-1">
-                    back
+                    <div className="card__cta">
+                      <div className="card__price-box">
+                        <p className="card__price-only">Only</p>
+                        <p className="card__price-value">$297</p>
+                      </div>
+                      <a href="#" className="btn btn--white">
+                        Book now!
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-1-of-3"></div>
-              <div className="col-1-of-3"></div>
+              <div className="col-1-of-3">
+                <div className="card">
+                  <div className="card__side card__side--front">
+                    <div className="card__picture card__picture--2">&nbsp;</div>
+                    <h4 className="card__heading">
+                      <span className="card__heading-span card__heading-span--2">
+                        The Forest Hicker
+                      </span>
+                    </h4>
+                    <div className="card__details">
+                      <ul>
+                        <li>7 day tours</li>
+                        <li>Up to 40 people</li>
+                        <li>6 tour guides</li>
+                        <li>Sleep in provided tents</li>
+                        <li>Difficulty: medium</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="card__side card__side--back card__side--back-2">
+                    <div className="card__cta">
+                      <div className="card__price-box">
+                        <p className="card__price-only">Only</p>
+                        <p className="card__price-value">$497</p>
+                      </div>
+                      <a href="#" className="btn btn--white">
+                        Book now!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-of-3">
+                <div className="card">
+                  <div className="card__side card__side--front">
+                    <div className="card__picture card__picture--3">&nbsp;</div>
+                    <h4 className="card__heading">
+                      <span className="card__heading-span card__heading-span--3">
+                        The Snow Adventure
+                      </span>
+                    </h4>
+                    <div className="card__details">
+                      <ul>
+                        <li>5 day tours</li>
+                        <li>Up to 15 people</li>
+                        <li>3 tour guides</li>
+                        <li>Sleep in cozy hotels</li>
+                        <li>Difficulty: hard</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="card__side card__side--back card__side--back-3">
+                    <div className="card__cta">
+                      <div className="card__price-box">
+                        <p className="card__price-only">Only</p>
+                        <p className="card__price-value">$897</p>
+                      </div>
+                      <a href="#" className="btn btn--white">
+                        Book now!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="u-center-text u-margin-top-huge">
+              <a href="#" className="btn btn--green">
+                Discover all tours
+              </a>
             </div>
           </section>
         </main>
