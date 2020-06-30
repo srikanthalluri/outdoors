824770e34c9fa7518d9a59ef0e152b3495ade1fe
Srikanth <srikanth.varma@gmail.com>
2020-06-30 21:11:21 +0530

    Designed the front part of the card in tours section

--- src/index.js
+++ src/index.js
@@ -166,15 +166,29 @@ class App extends React.Component {
 
           <section className="section-tours">
             <div className="u-center-text u-margin-bottom-big">
-              <h1 className="heading-secondary">
-                Exciting tours for adventurous people
-              </h1>
+              <h1 className="heading-secondary">most popular tours</h1>
             </div>
 
             <div className="row">
               <div className="col-1-of-3">
                 <div className="card">
-                  <div className="card__side card__side--front"></div>
+                  <div className="card__side card__side--front">
+                    <div className="card__picture card__picture--1">&nbsp;</div>
+                    <h4 className="card__heading">
+                      <span className="card__heading-span card__heading-span--1">
+                        The Sea Explorer
+                      </span>
+                    </h4>
+                    <div className="card__details">
+                      <ul>
+                        <li>3 day</li>
+                        <li>Up to 30 people</li>
+                        <li>2 tour guides</li>
+                        <li>Sleep in cozy hotels</li>
+                        <li>Difficulty: easy</li>
+                      </ul>
+                    </div>
+                  </div>
                   <div className="card__side card__side--back card__side--back-1">
                     back
                   </div>
